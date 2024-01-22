e0d7111c05e49fb5ef13fb3cf9f7d4e2db185591
Julian van D <73965544+meFqulted@users.noreply.github.com>
2024-01-22 20:42:17 +0100

    Had logs in it still :poop:

--- commands/utilities/botinfo.js
+++ commands/utilities/botinfo.js
@@ -6,7 +6,6 @@ module.exports = {
     .setDescription('Shows the bot info'),
 
   async execute(interaction) {
-    console.log(interaction)
     try {
       await interaction.reply('This bot is made in Node.js! Its source code can be found at https://github.com/CodeSpiral/discord-bot-base-js');
     } catch (error) {
