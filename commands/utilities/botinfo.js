6520b440c18780af0a34a9e235a5e432e7fa23d2
Julian van D <73965544+meFqulted@users.noreply.github.com>
2024-01-23 13:50:26 +0100

    Idk, extra credits or some.

--- commands/utilities/botinfo.js
+++ commands/utilities/botinfo.js
@@ -7,7 +7,7 @@ module.exports = {
 
   async execute(interaction) {
     try {
-      await interaction.reply('This bot is made in Node.js! Its source code can be found at https://github.com/CodeSpiral/discord-bot-base-js');
+      await interaction.reply('This bot is made in Node.js! Its source code can be found at [https://github.com/CodeSpiral/discord-bot-base-js](https://github.com/CodeSpiral/discord-bot-base-js). It was made by [CodeSpiral](https://github.com/CodeSpiral).');
     } catch (error) {
       console.error(error);
     }
